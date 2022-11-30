7e34bde904c55d80bbc4204c36b92c21812ebcbc
PeteChicchetti <pj0524@gmail.com>
2022-11-30 15:26:21 -0500

    added module rules

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,7 +27,20 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
